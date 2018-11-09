e092fa435af71e96dc17e5d06ec220773fe6a0ab
feelingit <nodejsgirl@gmail.com>
2018-11-10 02:53:30 +0300

    Update build/__tests__/testEventEmitter.js

--- build/__tests__/testEventEmitter.js
+++ build/__tests__/testEventEmitter.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const { expect } = require('chai');
 const { EventEmitter } = require('../EventEmitter');
 describe(`EventEmitter`, () => {
